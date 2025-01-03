2c3392bbf328d2456853a64089db11eb213f3f2f
Isabel Paredes <isabel.paredes@quantstack.net>
2025-01-03 12:45:52 +0100

    Update fribidi

--- recipes/recipes_emscripten/fribidi/build.sh
+++ recipes/recipes_emscripten/fribidi/build.sh
@@ -1,8 +1,27 @@
+#!/bin/bash
+
+set -ex
+
 export CFLAGS="$CFLAGS -fPIC"
+export LDFLAGS="$LDFLAGS -sALLOW_MEMORY_GROWTH=1 -sSTACK_SIZE=1MB"
 
-emconfigure ./configure \
-    --enable-shared=no \
-    --enable-static=yes \
-    --prefix=$PREFIX
+# Bug: there is no symlink to emcc.py
+if [ ! -f $BUILD_PREFIX/bin/emcc.py ]; then
+    echo "Creating symlink to emcc.py"
+    ln -s $BUILD_PREFIX/opt/emsdk/upstream/emscripten/emcc.py $BUILD_PREFIX/bin/emcc.py
+fi
 
-emmake make install
+meson_setup_args=(
+    -Ddocs=false
+    -Dtests=false
+    -Ddefault_library=static
+)
+
+meson setup builddir \
+    ${meson_setup_args[@]} \
+    --prefix=$PREFIX \
+    --buildtype=release \
+    --prefer-static \
+    --cross-file=$RECIPE_DIR/emscripten.meson.cross
+
+meson install -C builddir
